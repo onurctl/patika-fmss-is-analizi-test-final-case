d4e55583b1d5da58581efbc5676eea25e2bab24e
onurctl <77632313+onurctl@users.noreply.github.com>
2024-08-18 21:37:45 +0300

    Update AccountTypePage.java

--- bireysel/appiumTest/src/test/java/pages/AccountTypePage.java
+++ bireysel/appiumTest/src/test/java/pages/AccountTypePage.java
@@ -5,13 +5,18 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+
 public class AccountTypePage {
 
     private AppiumDriver <MobileElement> driver;
+    
     public AccountTypePage (AppiumDriver<MobileElement> driver) {
         this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
+
+    // locators:
+    
     @AndroidFindBy(id = "com.rentfly.app:id/chooseAccountTypeText")
     @iOSXCUITFindBy(accessibility = "choose_account_type_text")
     private MobileElement chooseAccountTypeText;
@@ -24,7 +29,8 @@ public class AccountTypePage {
     @iOSXCUITFindBy(accessibility = "company_button")
     private MobileElement companyButton;
 
-    // Methods
+    // functions:
+    
     public void clickPersonalButton() {
         personalButton.click();
     }
@@ -37,8 +43,8 @@ public class AccountTypePage {
     public String getChooseAccountTypeText() {
         return chooseAccountTypeText.getText();
     }
-
     */
+    
     public String getPersonalButtonText() {
         return personalButton.getText();
     }
